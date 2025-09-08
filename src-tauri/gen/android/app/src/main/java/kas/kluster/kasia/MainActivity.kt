ada8f911ce981791f99081e3e695e5c085c82b2d
IzioDev <9900846+IzioDev@users.noreply.github.com>
2025-09-08 19:49:21 +0200

    wip: fix edge to edge on android 15

--- src-tauri/gen/android/app/src/main/java/kas/kluster/kasia/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/kas/kluster/kasia/MainActivity.kt
@@ -1,5 +1,6 @@
 package kas.kluster.kasia
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.WindowCompat
@@ -9,10 +10,9 @@ class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
-
-    WindowCompat.setDecorFitsSystemWindows(window, true)
-
-    val c = WindowInsetsControllerCompat(window, window.decorView)
-    c.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+//    WindowCompat.setDecorFitsSystemWindows(window, true)
+//
+//    val c = WindowInsetsControllerCompat(window, window.decorView)
+//    c.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
   }
 }
