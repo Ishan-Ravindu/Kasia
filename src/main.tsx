acb8f089bc9a916b6f713027e333e133b84d4f47
IzioDev <9900846+IzioDev@users.noreply.github.com>
2025-09-23 15:47:04 +0200

    chore: add app version and commit sha visually

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { type Root } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
 export async function loadApplication(root: Root) {
+  console.log(__APP_VERSION__);
+  console.log(__COMMIT_SHA__);
   root.render(
     <BrowserRouter>
       <App />
