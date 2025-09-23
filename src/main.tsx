86b28fe46e6f51c51f004e2ef6a1bfa69480e1f0
IzioDev <9900846+IzioDev@users.noreply.github.com>
2025-09-23 15:48:26 +0200

    clean: remove logs

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { type Root } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
 export async function loadApplication(root: Root) {
-  console.log(__APP_VERSION__);
-  console.log(__COMMIT_SHA__);
   root.render(
     <BrowserRouter>
       <App />
